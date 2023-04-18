9c006ac18007abbe5b1196cefe1af6add9d1f6a1
Piekielniok <piekielniok@wp.pl>
2023-04-19 00:24:04 +0200

    perrito ale już działa

--- content.js
+++ content.js
@@ -1,2 +1,5 @@
-var imgElement = document.querySelector('img[alt="Extranet - WSB Gdańsk"]');
-imgElement.src = 'https://raw.githubusercontent.com/Piekielniok/perrito-extension/master/layout_set_logo.png';
+var images = document.querySelectorAll('img[src*="/image/layout_set_logo"]');
+
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'https://raw.githubusercontent.com/Piekielniok/perrito-extension/master/layout_set_logo.png';
+}
