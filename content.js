9938ffeb702819b367c883a03332e172a44b19fc
Piekielniok <piekielniok@wp.pl>
2023-04-18 23:25:15 +0200

    najlepsze perrito

--- content.js
+++ content.js
@@ -1,2 +1,2 @@
 var imgElement = document.querySelector('img[alt="MyImage"]');
-imgElement.src = 'https://drive.google.com/file/d/1K0s_NDyKuqYNr2PqF1bPr1GO4UUviAf5/view?usp=sharing';
+imgElement.src = 'https://raw.githubusercontent.com/Piekielniok/perrito-extension/master/layout_set_logo.png';
